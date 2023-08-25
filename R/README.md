22662632fd5f81637bbc90d3748d7f9b32c3237c
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-08-25 16:29:10 +0100

    Add docs

---
+++ R/README.md
@@ -0,0 +1,4 @@
+# R code
+
+This directory contains the [R code](https://r-pkgs.org/code.html) for this package.
+
--- R/main.R
+++ R/main.R
@@ -1,20 +1,28 @@
-main <- function() {
+#' Run workflow
+#'
+#' @description
+#' `main` executes the entire data pipeline for all data sets.
+#'
+#' @details
+#' TODO
+#'
+#' @param root_directory String. Path. The directory that contains all the working directories.
+#' 
+#' @examples
+#' run_workflow("ae", "/mnt/data/cured")
+#' 
+main <- function(root_directory) {
 
-  # TODO
-  # Define working directory
-  root_directory <- "/mnt/data/cured/"
-  
-  # Iterate over data sets
+  # A list of the unique identifier of each of the data sets to work with.
   data_set_ids <- c("apc", "ae", "op")
 
-  # Run each workflow
+  # Iterate over data sets
   for (data_set_id in data_set_ids) {
+    # Run each workflow
     run_workflow(
       data_set_id=data_set_id,
       root_directory=root_directory
     )
   }
-  
 
-  
 }
