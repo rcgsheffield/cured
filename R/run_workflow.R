06aa47b630b54e201acb3431aa6f69d7b584843a
Joe Heffer <60133133+Joe-Heffer-Shef@users.noreply.github.com>
2023-08-25 13:55:12 +0100

    Update run_workflow.R
    Sketch out workflow

--- R/run_workflow.R
+++ R/run_workflow.R
@@ -1,3 +1,30 @@
-run_workflow <- function() {
+run_workflow <- function(data_set_id, root_directry) {
+  # TODO
+  # Parse the TOS
+  tos_path = "/path/to/TOS.xlsx"
+  # Get data types for each column
+  metadata <- parse_tos(tos_path)
+
+  # Define raw data directory
+  raw_data_dir = cat(root_directory, "/", data_set_id, "01-raw")
+
+  # Convert to binary format
+  staging_dir <- csv_to_binary_format(raw_data_dir, metadata)
+
+  # Validate
+  validate(staging_dir, metadata)
+
+  # Generate summary report
+  summarise(staging_dir, metadata)
+  
+  # Data linkage
+
+  # Cleaning
+
+  # Data quality rules (Flag "bad" records)
+
+  # Generate data quality report
+
+  # Generate FHIR data model
   
 }
