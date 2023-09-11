1abe15d9dbad169afc32d1604621a181d431e629
Joe Heffer <j.heffer@sheffield.ac.uk>
2023-09-11 16:22:43 +0100

    Fix linting errors

--- R/run_workflow.R
+++ R/run_workflow.R
@@ -11,38 +11,34 @@ library(cli)
 #' @param data_set_id String. Data set identifier e.g. "apc" or "op"
 #' @param root_directry String. The root directory that contains all the data.
 run_workflow <- function(data_set_id, root_directry) {
-  
   # Cast parameters to the correct data type
   data_set_id <- as.character(data_set_id)
   root_directry <- file.path(root_directry)
-  
+
   # Check whether the data directory exists
   if (!file.exists(root_directory)) {
     cli::cli_alert_warning("Directory doesn't exist '{root_directory}'")
     stop("Data directory not found")
   }
-  
+
   cli::cli_alert_info("Root directory '{root_directory}'")
-  
+
   # TODO
   # Parse the TOS
-  tos_path = file.path("/path/to/TOS.xlsx")
+  tos_path <- file.path("/path/to/TOS.xlsx")
   # Get data types for each column
-  #metadata <- parse_tos(tos_path)
 
   # Define working directories
-  data_set_dir = file.path(root_directory, "/", data_set_id)
-  raw_data_dir = file.path(data_set_dir, "/01-raw")
+  data_set_dir <- file.path(root_directory, "/", data_set_id)
+  raw_data_dir <- file.path(data_set_dir, "/01-raw")
 
   # Convert to binary format
-  #staging_dir <- csv_to_binary(raw_data_dir, metadata)
+  staging_dir <- csv_to_binary(raw_data_dir, metadata)
 
   # Validate
-  #validate(staging_dir, metadata)
 
   # Generate summary report
-  #summarise(staging_dir, metadata)
-  
+
   # Data linkage
 
   # Cleaning
@@ -54,5 +50,4 @@ run_workflow <- function(data_set_id, root_directry) {
   # Generate data quality report
 
   # Generate FHIR data model
-  
 }
